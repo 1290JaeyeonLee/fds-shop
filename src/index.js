b3287249d56ed021a78996fa66ac85877ac48aa1
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-06-04 00:26:42 +0900

    주문내역페이지 완료, 상세페이지 바로구매 기능구현

--- src/index.js
+++ src/index.js
@@ -34,7 +34,8 @@ const templates = {
   orderShip: document.querySelector('#order-ship').content,
   orderHistory: document.querySelector('#order-history').content,
   orderHisList: document.querySelector('#order-history__list').content,
-  orderHisNoList: document.querySelector('#order-history__nolist').content      
+  orderHisNoList: document.querySelector('#order-history__nolist').content,      
+  orderProduct: document.querySelector('#order-product').content  
 }
 
 function login(token) {
@@ -73,6 +74,9 @@ async function memberInfo() {
     })
     frag.querySelector('.member__btn-cart').addEventListener('click', e => {
       cartPage();
+    })
+    frag.querySelector('.member__btn-order').addEventListener('click', e => {
+      historyPage()
     })
   } else {
     frag.querySelector('.member__btn-login').addEventListener('click', e => {
@@ -283,28 +287,60 @@ async function contentPage(productId){
   
   const contentEl = frag.querySelector('.product-details__content');
   contentEl.textContent = res.data.content;    
-  frag.querySelector('.product-details__btn-list').addEventListener('click', e => {
+  frag.querySelector('.product-details__btn-golist').addEventListener('click', e => {
     indexPage();
   })
 
   frag.querySelector('.product-details__btn-cart').addEventListener('click', async e => {
     e.preventDefault();
-    const payload = {
-      size : 0,
-      quantity : 0,
-      price: 0,
-      productId : productId
-    } 
-    for(let i = 0; i < items.length; i++){
-      payload.size = items[i].size;
-      payload.quantity = items[i].quantity;      
-      payload.price = items[i].price;
+    if(memberEl.classList.contains('authed')){
+      const payload = {
+        size : 0,
+        quantity : 0,
+        price: 0,
+        productId : productId
+      } 
+      for(let i = 0; i < items.length; i++){
+        payload.size = items[i].size;
+        payload.quantity = items[i].quantity;      
+        payload.price = items[i].price;
 
-      const cartRes = await shopAPI.post('/carts', payload);
-      cartPage();
+        const cartRes = await shopAPI.post('/carts', payload);
+        cartPage();
+      }
+    } else {
+      alert('로그인 후 이용해주세요.');
+      loginPage();
     } 
   })
+
+  frag.querySelector('.product-details__btn-buy').addEventListener('click', async e => {
+    e.preventDefault();
+    if(memberEl.classList.contains('authed')){
+      const payload = {
+        size : 0,
+        quantity : 0,
+        price: 0,
+        productId : productId
+      } 
+      for(let i = 0; i < items.length; i++){
+        payload.size = items[i].size;
+        payload.quantity = items[i].quantity;      
+        payload.price = items[i].price;
+        const cartRes = await shopAPI.post('/carts', payload);
+        noCart();
+      }
+    } else {
+      alert('로그인 후 이용해주세요.');
+      loginPage();
+    } 
+  })
+
   render(frag);
+}
+async function noCart(){
+  const res = await shopAPI.get('/carts?_expand=product');  
+  orderPage(res.data.map(cart => cart.id));
 }
 
 async function cartPage(){
@@ -394,18 +430,6 @@ async function cartPage(){
   }
   orderBtnEl.addEventListener('click', async e => {
     e.preventDefault();
-    
-    const payload = {
-      cartId : 0
-    } 
-    //const orderRes2 = await shopAPI.delete(`/orders`);  
-    for(let i = 0; i < res.data.length; i++){
-      payload.cartId = res.data[i].id;
-      //payload.productId = cart.productId;
-      const orderRes = await shopAPI.post('/orders', payload);
-      //const orderRes = await shopAPI.post(`/cart/${cartId}/orders`, payload);
-      ///products/${productId}/carts
-    }
     orderPage(res.data.map(cart => cart.id));
   })
   render(frag);
@@ -413,60 +437,118 @@ async function cartPage(){
 
 async function orderPage(cartIds){
   const carts = [];
+
   for (const cartId of cartIds) {
-    const res = await shopAPI.get(`/carts/${cartId}`);
+    const res = await shopAPI.get(`/carts/${cartId}?_expand=product`);
     carts.push(res.data);
   }
+
   const frag = document.importNode(templates.order, true);
   const orderListEl = frag.querySelector('.order__list');
   const orderShipEl = frag.querySelector('.order__ship');
   const totalPay = frag.querySelector('.order__pay__price-num');
+  const payBtnEl = frag.querySelector('.order__pay__btn');
 
   let countPrice = 0;
-  // carts.forEach(async order => {
-    const productRes = await shopAPI.get(`/carts?_expand=product`);
-    productRes.data.forEach(item => {
-      console.log(item)
-
-      const orderFrag = document.importNode(templates.orderList, true);
-      const imgEl = orderFrag.querySelector('.order__item__img');
-      imgEl.setAttribute('src', item.product.img);
-      const titleEl = orderFrag.querySelector('.order__item__title');
-      titleEl.textContent = item.product.product;
-      const sizeEl = orderFrag.querySelector('.order__item__size');
-      sizeEl.textContent = item.size;
-      const quantityEl = orderFrag.querySelector('.order__item__quantity');
-      quantityEl.textContent = item.quantity;
-      const priceEl = orderFrag.querySelector('.order__item__price');
-      priceEl.textContent = item.price;
-      countPrice += item.price;
-      orderListEl.appendChild(orderFrag);
-    })
-    totalPay.textContent = countPrice;
-    
-  // })
+  carts.forEach(order => {
+    const orderFrag = document.importNode(templates.orderList, true);
+    const imgEl = orderFrag.querySelector('.order__item__img');
+    imgEl.setAttribute('src', order.product.img);
+    const titleEl = orderFrag.querySelector('.order__item__title');
+    titleEl.textContent = order.product.product;
+    const sizeEl = orderFrag.querySelector('.order__item__size');
+    sizeEl.textContent = order.size;
+    const quantityEl = orderFrag.querySelector('.order__item__quantity');
+    quantityEl.textContent = order.quantity;
+    const priceEl = orderFrag.querySelector('.order__item__price');
+    priceEl.textContent = order.price;
+    countPrice += order.price;
+    orderListEl.appendChild(orderFrag);
+  })
+  totalPay.textContent = countPrice;
+  
   const userRes = await shopAPI.get(`/carts?_expand=user`);
   const shipFrag = document.importNode(templates.orderShip, true);
   userRes.data.forEach(user => {
-    console.log(user)
     const orderShipBox = shipFrag.querySelector('.order__ship__box');
-
-    const orderShipName = shipFrag.querySelector('.order__ship__name');
+    const orderShipName = shipFrag.querySelector('#order__ship__name');
     orderShipName.setAttribute('value', user.user.name);
-
-    const orderShipAddress = shipFrag.querySelector('.order__ship__address');
+    const orderShipAddress = shipFrag.querySelector('#order__ship__address');
     orderShipAddress.setAttribute('value', user.user.address);
-
-    const orderShipPhone = shipFrag.querySelector('.order__ship__phone');
+    const orderShipPhone = shipFrag.querySelector('#order__ship__phone');
     orderShipPhone.setAttribute('value', user.user.phone);
-    
   })
   orderShipEl.appendChild(shipFrag);
 
+  payBtnEl.addEventListener('click', async e => {
+    e.preventDefault();
+    alert('결제가 완료되었습니다.');
+    const payDate = new Date();
+
+    const payload = {
+      orderItems : [],
+      date: payDate.toLocaleString,
+      payTotal: countPrice
+    }
+
+    carts.forEach(cart => {
+      let orderItem = {
+        size : cart.size,
+        quantity : cart.quantity,      
+        price : cart.price,
+        productId : cart.productId
+      }
+      console.log(payload)
+      payload.orderItems.push(orderItem);
+    });
+    console.log(payload)
+    const orderRes = await shopAPI.post('/orders', payload);
+    historyPage();
+  })
   render(frag);
 }
 
+async function historyPage(){
+  //const cartDel = await shopAPI.delete('/carts');
+  const res = await shopAPI.get('/orders');
 
+  console.log(res.data)
+  const frag = document.importNode(templates.orderHistory, true);
+  const historyListEl = frag.querySelector('.order-history__list');
+  if(res.data.length === 0){
+    const orderFrag = document.importNode(templates.orderHisNoList, true);
+    historyListEl.appendChild(orderFrag);
+  } else {
+    res.data.forEach(async order => {
+      const orderFrag = document.importNode(templates.  orderHisList, true);
+      const orderNumEl = orderFrag.querySelector('.order-history__number');
+      orderNumEl.textContent = order.id;
+      const dateEl = orderFrag.querySelector('.order-history__date');
+      dateEl.textContent = order.date;
+      const priceEl = orderFrag.querySelector('.order-history__total');
+      priceEl.textContent = order.payTotal;
+      const itemListEl = orderFrag.querySelector('.order-history__products');
+      
+
+      // order.orderItems.forEach(item => {
+      //   const itemFrag = document.importNode(templates.orderProduct, true);
+      //   const productEl = itemFrag.querySelector('.order-history__name');
+      //   productEl.textContent = product;
+      //   const sizeEl = itemFrag.querySelector('.order-history__size');
+      //   sizeEl.textContent = size;
+      //   const quantityEl = itemFrag.querySelector('.order-history__quantity');
+      //   quantityEl.textContent = quantity;
+      //   const priceEl = itemFrag.querySelector('.order-history__price');
+      //   priceEl.textContent = price;
+      //   itemListEl.appendChild(itemFrag);
+      // }) 
+  
+      historyListEl.appendChild(orderFrag);
+
+    });
+  }
+  render(frag);
+}
 
 
 document.querySelector('.header__heading').addEventListener('click', e => {
